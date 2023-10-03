9cda13db4650e62260ab1ce455b66905cff98838
nibir03 <nivir46@gmail.com>
2023-10-03 15:58:23 +0200

    först

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
-
+ 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -15,6 +15,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.status(200).send('hello');
+});
+
+
+
+app.get('/about', (req, res) => {
+  res.status(200).send('about page');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
