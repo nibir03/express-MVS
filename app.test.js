beb135a808ee35399f44eda82d397f1b5a1f6649
herkommer <herkommer@users.noreply.github.com>
2023-10-03 12:51:28 +0200

    justerade de inledande testerna som vägledning

--- app.test.js
+++ app.test.js
@@ -1,9 +1,23 @@
 const request = require('supertest');
 const app = require('./app');
 
-test('GET /', done => {
+test('I am alive and responding', done => {
     request(app)
     .get('/')
     .expect(200)
+    .end(done)
+});
+
+test('GET /', done => {
+    request(app)
+    .get('/about')
+    .expect(200)
+    .end(done)
+});
+
+test('GET jibberish', done => {
+    request(app)
+    .get('/asjdgajdsg')
+    .expect(404)
     .end(done)
 });
